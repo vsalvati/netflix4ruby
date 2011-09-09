bc60aa473e16b0756d47a04a75a8e0069ef5b33e
Nick Veys <nveys@aramisgroup.com>
2011-09-08 23:50:48 -0500

    Adding test to catalog title builder

--- test/test_catalog_title_builder.rb
+++ test/test_catalog_title_builder.rb
@@ -4,8 +4,14 @@ class TestCatalogTitleBuilder < Test::Unit::TestCase
 
   file = File.dirname(__FILE__) + '/catalog_titles.xml'
 
+  should 'create the expected number of titles' do
+    queue_items = Netflix4Ruby::Builders::CatalogTitleBuilder.from_file file
+    assert_equal 25, queue_items.count
+  end
+
   should 'create a catalog title with basic attributes' do
     catalog_titles = Netflix4Ruby::Builders::CatalogTitleBuilder.from_file file
+
     catalog_title = catalog_titles[0]
     assert_equal 'http://api.netflix.com/catalog/titles/movies/60023642', catalog_title.id_url
     assert_equal '60023642', catalog_title.id
@@ -14,6 +20,7 @@ class TestCatalogTitleBuilder < Test::Unit::TestCase
 
   should 'create a catalog title with box art urls' do
     catalog_titles = Netflix4Ruby::Builders::CatalogTitleBuilder.from_file file
+
     catalog_title = catalog_titles[0]
     assert_equal 'http://cdn-2.nflximg.com/en_us/boxshots/tiny/60023642.jpg', catalog_title.box_art_small
     assert_equal 'http://cdn-2.nflximg.com/en_us/boxshots/small/60023642.jpg', catalog_title.box_art_medium
